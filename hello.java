4c4a476374c751bc898f0f6c3293cc8c55f574bf
Pradumn88 <ishup3658@gmail.com>
2025-06-01 17:00:03 +0530

    modified after cloning

--- hello.java
+++ hello.java
@@ -3,4 +3,10 @@ class Myfirst
     public static void main(String[] args) {
         System.out.println("hello world!");
     }
+    public static void sum(int a,int b){
+        a = 10;
+        b = 20;
+        int sum = a+b;
+        System.out.println("sum is "+sum);
+    }
 }
